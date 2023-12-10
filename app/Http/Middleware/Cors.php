8668caa6fc379fb14ffc4a01cfe19b5f9926776a
Job462 <jobmolina81@gmail.com>
2023-12-10 01:27:26 -0400

    cuarto

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Cors
+{
+    protected $headers = [
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE',
+        'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
+    ];
+    
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request);
+    }
+}
