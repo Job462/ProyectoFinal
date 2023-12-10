02b1485dbe8a5f3a2d0fb38fd1a0e1e11967fa8b
Job462 <jobmolina81@gmail.com>
2023-12-10 03:44:32 -0400

    a

--- database/migrations/2023_12_10_014250_create_contacts_table.php
+++ database/migrations/2023_12_10_014250_create_contacts_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('nombre');
             $table->string('celular',10);
             $table->string('email')->unique();
-            $table->timestamps();
 
         });
     }
